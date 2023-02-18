1dbb33a273612890b3f9b7179b2cfd1db260032c
oudsoft <oudsoft@gmail.com>
2023-02-18 09:43:44 +0700

    update

---
+++ src/components/Pagination.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import "./Pagination.css";
+
+const Pagination = ({ value }) => {
+
+  return (
+    <div className="PaginationMain">
+    </div>
+  );
+};
+
+export default Pagination;
